67b77ac4a09e6fa6c7c76ea18e98b5e739512439
Melroy Noronha <melmax1208@gmail.com>
2022-02-21 21:10:49 +0530

    setup useDispatch and useSelector with TypeScript

--- src/app/store.ts
+++ src/app/store.ts
@@ -7,3 +7,6 @@ export const store = configureStore({
     counter: counterReducer
   }
 });
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
