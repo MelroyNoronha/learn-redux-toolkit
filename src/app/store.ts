321f9fbdde6e3e9fef27d6f350f78b3f77c4d1dd
Melroy Noronha <melmax1208@gmail.com>
2022-02-24 23:46:32 +0530

    setup middleware for apiSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import counterReducer from '../features/counter/counterSlice';
+import { apiSlice } from '../features/api/apiSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer
-  }
+    counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware)
 });
 
 export type AppDispatch = typeof store.dispatch;
